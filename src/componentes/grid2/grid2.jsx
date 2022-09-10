5583a9fd39727d3c85cdbdf98f7edbb688a7b1f3
patriky1 <patrikybrito@gmail.com>
2022-09-10 19:24:31 -0300

    first commit

---
+++ src/componentes/grid2/grid2.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import "./grid2.css";
+function grid2() {
+  return (
+    <>
+      <div className="container">
+        <div className="card">
+          <div className="content">
+            <div className="imgBx">
+              <img src="https://image.flaticon.com/icons/png/256/4213/4213732.png" />
+            </div>
+            <div className="contentBx">
+              <h3>
+                Lion Happy Birthday
+              </h3>
+            </div>
+          </div>
+          <ul className="sci">
+            <li>
+              <a href="">happy</a>
+            </li>
+            <li>
+              <a href="">birth</a>
+            </li>
+            <li>
+              <a href="">day</a>
+            </li>
+          </ul>
+        </div>
+        <div className="card">
+          <div className="content">
+            <div className="imgBx">
+              <img src="https://image.flaticon.com/icons/png/256/4213/4213736.png" />
+            </div>
+            <div className="contentBx">
+              <h3>
+                FrogHappy Birthday
+              </h3>
+            </div>
+          </div>
+          <ul className="sci">
+            <li>
+              <a href="">happy</a>
+            </li>
+            <li>
+              <a href="">birth</a>
+            </li>
+            <li>
+              <a href="">day</a>
+            </li>
+          </ul>
+        </div>
+        <div className="card">
+          <div className="content">
+            <div className="imgBx">
+              <img src="https://image.flaticon.com/icons/png/256/4213/4213641.png" />
+            </div>
+            <div className="contentBx">
+              <h3>
+                GiraffeHappy Birthday
+              </h3>
+            </div>
+          </div>
+          <ul className="sci">
+            <li>
+              <a href="">happy</a>
+            </li>
+            <li>
+              <a href="">birth</a>
+            </li>
+            <li>
+              <a href="">day</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default grid2;
